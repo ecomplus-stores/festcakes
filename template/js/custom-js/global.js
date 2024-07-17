1c1ac7d021e3e706e4120e642610a1a192fe8dad
Leonardo Matos <leomp120894@gmail.com>
2024-07-17 20:35:58 -0300

    Update global.js

--- template/js/custom-js/global.js
+++ template/js/custom-js/global.js
@@ -4,24 +4,28 @@ import EcomSearch from '@ecomplus/search-engine'
 const storeSpec = 'Fastcakes'
 
 EcomSearch.dslMiddlewares.push((dsl) => {
+  const storeFilter = {
+    nested: {
+      path: 'specs',
+      query: {
+        bool: {
+          filter: [{
+            term: { 'specs.grid': 'store' }
+          }, {
+            terms: { 'specs.text': [storeSpec] }
+          }]
+        }
+      }
+    }
+  }
+  const { filter } = (dsl.query && dsl.query.bool) || {}
+  if (filter) {
+    filter.push(storeFilter)
+    return
+  }
   merge(dsl, {
     query: {
-      bool: {
-        filter: [{
-          nested: {
-            path: 'specs',
-            query: {
-              bool: {
-                filter: [{
-                  term: { 'specs.grid': 'store' }
-                }, {
-                  terms: { 'specs.text': [storeSpec] }
-                }]
-              }
-            }
-          }
-        }]
-      }
+      bool: { filter: [storeFilter] }
     }
   })
 })
