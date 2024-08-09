aedf31ac209ecba03a1e44a36a459111c0a7c41c
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2024-08-09 13:58:46 -0300

    Update global.js

--- template/js/custom-js/global.js
+++ template/js/custom-js/global.js
@@ -1,31 +1,31 @@
+import * as merge from 'lodash.merge'
 import EcomSearch from '@ecomplus/search-engine'
 
-const fixCategoryIdsFilter = ({ terms }) => {
-  if (
-    terms &&
-    terms['categories.name'] &&
-    /^[0-9a-f]{24}$/.test(terms['categories.name'][0])
-  ) {
-    terms['categories._id'] = terms['categories.name']
-    delete terms['categories.name']
-  }
-}
+const storeSpec = 'Festcakes'
 
 EcomSearch.dslMiddlewares.push((dsl) => {
-  if (dsl.query && dsl.query.bool) {
-    if (dsl.query.bool.filter) {
-      dsl.query.bool.filter.forEach(fixCategoryIdsFilter)
-    }
-    if (dsl.query.bool.must) {
-      dsl.query.bool.must.forEach((filter) => {
-        if (filter.multi_match) {
-          const { fields } = filter.multi_match
-          if (Array.isArray(fields)) {
-            fields.push('skus')
-          }
+  const storeFilter = {
+    nested: {
+      path: 'specs',
+      query: {
+        bool: {
+          filter: [{
+            term: { 'specs.grid': 'store' }
+          }, {
+            terms: { 'specs.text': [storeSpec] }
+          }]
         }
-        fixCategoryIdsFilter(filter)
-      })
+      }
     }
   }
+  const { filter } = (dsl.query && dsl.query.bool) || {}
+  if (filter) {
+    filter.push(storeFilter)
+    return
+  }
+  merge(dsl, {
+    query: {
+      bool: { filter: [storeFilter] }
+    }
+  })
 })
